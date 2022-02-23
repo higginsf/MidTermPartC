22833522ea2c3594b6534af9afd85479d7608ab4
higginsf <higginsf@sheridancollege.ca>
2022-02-23 11:05:56 -0500

    Merge origin/master
    Conflicts:
    	Documents/NetBeansProjects/StudentList/src/studentlist/Student.java

--- src/midtermexam_versiona_extensioncode/TestUserprofile.java
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package midtermexam_versiona_extensioncode;
-
+import java.util.*;
 /**
  *
  * @author Fallon Higgins
@@ -12,6 +12,33 @@ public class TestUserprofile {
     
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter your name: ");
+        String name = sc.next();
+        
+       System.out.println("Please choose a genre : ");
+
+        int option = 0;
+        String choice;
+        switch (option){
+            case 1: 
+                choice = "Comedy";
+                break;
+            case 2: 
+                choice = "Drama";
+                break;
+            case 3: 
+                choice = "Action";
+                break;
+            case 4: 
+                choice = "Mystery";
+                break;
+        }
+        
+    
+        
+        System.out.println("Thank your for creating a user profile");
     }
     
 }
